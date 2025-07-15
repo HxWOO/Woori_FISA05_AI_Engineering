77165445aca181a557ebdffc272fa9443ddf4a51
HxWOO <kimjd766@gmail.com>
2025-07-15 09:30:27 +0900

    혼공파 월별 카드 지출 내역 분석 추가 실습

--- self_python/ch_02/step_practice.py
+++ self_python/ch_02/step_practice.py
@@ -5,9 +5,36 @@ import seaborn as sns
 from step_1 import OUT_DIR
 from step_3_2 import OUT_3_2
 
-df_raw = pd.read_excel(OUT_3_2)
-df_raw.columns
-labels, values = df_raw["분류"], df_raw["누적금액"]
+plt.rcParams['font.family'] = 'Malgun Gothic'      # 한글 폰트 지정
+plt.rcParams['axes.unicode_minus'] = False         # 마이너스 기호 깨짐 방지
 
-sns.set_theme(context="poster", font="Malgun Gothic")
-sns.barplot(df_raw, x="분류", y="누적금액")
+def load_data2(n: int = 4) -> pd.DataFrame:
+    df_raw = pd.read_excel(OUT_3_2)
+    df_head, df_tail = df_raw[:n], df_raw[n:]
+    df_sum = df_tail.drop(columns="분류").sum().to_frame().T
+    df_sum["분류"] = "기타"
+    return pd.concat((df_head, df_sum), ignore_index=True)
+
+def custom_y_tick(x):
+    return f"x:,"
+
+
+if __name__ == "__main__":
+    df = load_data2()
+    labels, values = df["분류"], df["누적금액"]
+    f, ax = plt.subplots(figsize=(16, 9), dpi=100)
+
+    sns.set_theme(context="poster", font="Malgun Gothic")
+    ax.bar(
+        labels,
+        values,
+        width=0.3
+    )
+
+    ax.yaxis.grid(True, linestyle='--', linewidth=1, color='gray')  # y축 점선 그리드
+    # y축 눈금 위치와 레이블 설정
+    ax.set_yticks(values)
+    ax.set_yticklabels(values.apply(lambda x: f"{x:,}"))
+
+    ax.set_title("분류별 누적 사용금액")
+    f.savefig(OUT_DIR/f"{Path(__file__).stem}.png")
